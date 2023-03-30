175168e50aea3661ce881ca1b987ac66d966d28c
CAETANO DARTORA ROQUE <caetano_roque@estudante.sesisenai.org.br>
2023-03-29 21:06:33 -0300

    feat: add two functions, addition and subtraction

---
+++ index.js
@@ -0,0 +1,8 @@
+const somar=(numero1, numero2)=>numero1+numero2
+const subtrair=(numero1,numero2)=>numero1-numero2
+// console.log(somar(1,2))
+// console.log(subtrair(5,2))
+module.exports = {
+    somar,
+    subtrair
+}
