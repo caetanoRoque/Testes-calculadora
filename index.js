9103a9227fd5e9a1defedbe38e32f5a093e5ad94
CAETANO DARTORA ROQUE <caetano_roque@estudante.sesisenai.org.br>
2023-03-29 22:01:42 -0300

    fix: fix the result

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const somar=(numero1, numero2)=>numero1+numero2
 const subtrair=(numero1,numero2)=>numero1-numero2
-// console.log(somar(1,2))
-// console.log(subtrair(5,2))
+console.log(somar(1,2))
+console.log(subtrair(5,2))
 module.exports = {
     somar,
     subtrair
